a0fef9266260fcb88be91f1bdfbf0b61c83a5bd5
ddenfi <deki.nfi@gmail.com>
2024-01-03 07:48:30 +0700

    add basic api route

---
+++ asyst-tect-test/src/app/api/flights/[id]/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export async function GET(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    console.log(params.id);
+    return NextResponse.json({ id: params.id })
+}
