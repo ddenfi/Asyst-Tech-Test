321970ff4fe4fa9d96064df4728288e07150c2bb
ddenfi <deki.nfi@gmail.com>
2024-01-03 12:41:43 +0700

    add CRUD to the api

--- asyst-tect-test/src/app/api/booking/route.ts
+++ asyst-tect-test/src/app/api/booking/route.ts
@@ -1,11 +1,46 @@
-import { getBookings } from "@/utils/db/service";
-import { NextResponse } from "next/server";
+import { deleteBookingById, getBooking } from "@/utils/data";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const data = await getBookings()
-        return NextResponse.json({ message: "success", data},{status:200})
-    }catch (err){
-        return NextResponse.json({ message: "success", err},{status:500})
+        const data = getBooking()
+        return NextResponse.json({ message: "success", data }, { status: 200 })
+    } catch (err) {
+        return NextResponse.json({ message: "success", err }, { status: 500 })
+    }
+}
+
+export async function DELETE(req: NextRequest) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get("id");
+
+        if (id) deleteBookingById(id); else throw Error("Booking Not Found")
+
+        return NextResponse.json({ message: "success", }, { status: 200 })
+    } catch (err) {
+        return NextResponse.json({ message: "error", err }, { status: 500 })
+    }
+}
+
+export async function UPDATE(req: NextRequest) {
+    try {
+
+    } catch (err) {
+        return NextResponse.json({ message: "error", err }, { status: 500 })
+    }
+}
+
+
+export async function POST(req: NextRequest) {
+    try {
+        req.formData
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get("id");
+
+
+
+    } catch (err) {
+        return NextResponse.json({ message: "error", err }, { status: 500 })
     }
 }
--- asyst-tect-test/src/utils/data.ts
+++ asyst-tect-test/src/utils/data.ts
@@ -1,85 +1,143 @@
 type Flight = {
-    flightId:String
-    flightNumber: String,
-    airline: String,
-    departureAirport: String,
-    arrivalAirport: String,
-    departureTime: String,
-    arrivalTime: String,
-    aircraft: String,
+  flightId: string
+  flightNumber: string,
+  airline: string,
+  departureAirport: string,
+  arrivalAirport: string,
+  departureTime: string,
+  arrivalTime: string,
+  aircraft: string,
 }
 
-let flights:Flight[] =[
-    {
-        flightId: "FD1001",
-        flightNumber: "AA1234",
-        airline: "American Airlines",
-        departureAirport: "LAX",
-        arrivalAirport: "JFK",
-        departureTime: "2024-01-05T10:00:00",
-        arrivalTime: "2024-01-05T16:30:00",
-        aircraft: "Boeing 737-800"
-      },
-      {
-        flightId: "FD1002",
-        flightNumber: "UA789",
-        airline: "United Airlines",
-        departureAirport: "SFO",
-        arrivalAirport: "ORD",
-        departureTime: "2024-01-05T12:30:00",
-        arrivalTime: "2024-01-05T18:45:00",
-        aircraft: "Airbus A320"
-      },
-      {
-        flightId: "FD1003",
-        flightNumber: "DL567",
-        airline: "Delta Air Lines",
-        departureAirport: "ATL",
-        arrivalAirport: "MIA",
-        departureTime: "2024-01-05T15:00:00",
-        arrivalTime: "2024-01-05T18:15:00",
-        aircraft: "Boeing 757"
-      },
-      {
-        flightId: "FD1004",
-        flightNumber: "LH456",
-        airline: "Lufthansa",
-        departureAirport: "FRA",
-        arrivalAirport: "LHR",
-        departureTime: "2024-01-05T08:30:00",
-        arrivalTime: "2024-01-05T09:45:00",
-        aircraft: "Airbus A321"
-      },
-      {
-        flightId: "FD1005",
-        flightNumber: "NH890",
-        airline: "All Nippon Airways",
-        departureAirport: "NRT",
-        arrivalAirport: "BKK",
-        departureTime: "2024-01-05T21:00:00",
-        arrivalTime: "2024-01-06T03:30:00",
-        aircraft: "Boeing 787-9"
-      },
-      {
-        flightId: "FD1006",
-        flightNumber: "EK234",
-        airline: "Emirates",
-        departureAirport: "DXB",
-        arrivalAirport: "SYD",
-        departureTime: "2024-01-05T01:30:00",
-        arrivalTime: "2024-01-06T15:45:00",
-        aircraft: "Airbus A380"
-      },
-      {
-        flightId: "FD1007",
-        flightNumber: "CA987",
-        airline: "Air China",
-        departureAirport: "PEK",
-        arrivalAirport: "LHR",
-        departureTime: "2024-01-05T13:45:00",
-        arrivalTime: "2024-01-05T18:00:00",
-        aircraft: "Boeing 777-300ER"
-      },
+type BookingModel = {
+  bookingName: string,
+  bookingId: string,
+  bookingDate: string,
+  bookingStatus: string,
+  bookingClass: string,
+  flightInfo: Flight
+}
+
+let flights: Flight[] = [
+  {
+    flightId: "FD1001",
+    flightNumber: "AA1234",
+    airline: "American Airlines",
+    departureAirport: "LAX",
+    arrivalAirport: "JFK",
+    departureTime: "2024-01-05T10:00:00",
+    arrivalTime: "2024-01-05T16:30:00",
+    aircraft: "Boeing 737-800"
+  },
+  {
+    flightId: "FD1002",
+    flightNumber: "UA789",
+    airline: "United Airlines",
+    departureAirport: "SFO",
+    arrivalAirport: "ORD",
+    departureTime: "2024-01-05T12:30:00",
+    arrivalTime: "2024-01-05T18:45:00",
+    aircraft: "Airbus A320"
+  },
+  {
+    flightId: "FD1003",
+    flightNumber: "DL567",
+    airline: "Delta Air Lines",
+    departureAirport: "ATL",
+    arrivalAirport: "MIA",
+    departureTime: "2024-01-05T15:00:00",
+    arrivalTime: "2024-01-05T18:15:00",
+    aircraft: "Boeing 757"
+  },
+  {
+    flightId: "FD1004",
+    flightNumber: "LH456",
+    airline: "Lufthansa",
+    departureAirport: "FRA",
+    arrivalAirport: "LHR",
+    departureTime: "2024-01-05T08:30:00",
+    arrivalTime: "2024-01-05T09:45:00",
+    aircraft: "Airbus A321"
+  },
+  {
+    flightId: "FD1005",
+    flightNumber: "NH890",
+    airline: "All Nippon Airways",
+    departureAirport: "NRT",
+    arrivalAirport: "BKK",
+    departureTime: "2024-01-05T21:00:00",
+    arrivalTime: "2024-01-06T03:30:00",
+    aircraft: "Boeing 787-9"
+  },
+  {
+    flightId: "FD1006",
+    flightNumber: "EK234",
+    airline: "Emirates",
+    departureAirport: "DXB",
+    arrivalAirport: "SYD",
+    departureTime: "2024-01-05T01:30:00",
+    arrivalTime: "2024-01-06T15:45:00",
+    aircraft: "Airbus A380"
+  },
+  {
+    flightId: "FD1007",
+    flightNumber: "CA987",
+    airline: "Air China",
+    departureAirport: "PEK",
+    arrivalAirport: "LHR",
+    departureTime: "2024-01-05T13:45:00",
+    arrivalTime: "2024-01-05T18:00:00",
+    aircraft: "Boeing 777-300ER"
+  },
 ];
 
+let bookingDb: BookingModel[] = [
+  {
+    bookingName: "deki",
+    bookingId: "BKNG-789456",
+    bookingClass: "First Class",
+    bookingDate: "2024-01-05T10:00:00",
+    bookingStatus: "Confirmed",
+    flightInfo: flights[1]
+  },
+  {
+    bookingName: "deki2",
+    bookingId: "BKNG-423421",
+    bookingClass: "First Class",
+    bookingDate: "2024-01-05T10:00:00",
+    bookingStatus: "Cancelledd",
+    flightInfo: flights[1]
+  },
+  {
+    bookingName: "deki3",
+    bookingId: "BKNG-123124",
+    bookingClass: "First Class",
+    bookingDate: "2024-01-05T10:00:00",
+    bookingStatus: "Cancelledd",
+    flightInfo: flights[2]
+  }
+]
+
 export const getFlights = () => flights;
+
+export const getBooking = () => bookingDb;
+
+export const deleteBookingById = (id: string) => {
+  const dataIndex = bookingDb.findIndex((data) => {
+    data.bookingId == id
+  });
+
+  bookingDb.splice(dataIndex, 1);
+}
+
+export const createBooking = (booking: BookingModel) => {
+  bookingDb.push(booking);
+}
+
+export const updateBooking = (booking: BookingModel) => {
+  const dataIndex = bookingDb.findIndex((data) => {
+    data.bookingId == booking.bookingId
+  });
+
+  bookingDb[dataIndex] = booking;
+}
